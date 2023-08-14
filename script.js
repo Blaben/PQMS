c6ee5706064709538b6ec6f8701c88fce868ed30
Blaben <blabensolutions@gmail.com>
2023-08-14 09:05:21 +0000

    changes

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const menu = document.querySelector(".dash-submenu-items");
-const menuItems = document.querySelectorAll(".dash-submenu");
-const subMenuTitle = document.querySelectorAll(".dash-submenu-items .dash-submenu-title")
+// const menu = document.querySelector(".dash-submenu-items");
+// const menuItems = document.querySelectorAll(".dash-submenu");
+// const subMenuTitle = document.querySelectorAll(".dash-submenu-items .dash-submenu-title")
 
-menuItems.forEach((item,index) => {
-    item.addEventListener("click", () =>{
-        menu.classList.add("submenu-active");
-        item.classList.add("show-dash-submenu-items");
-        menuItems.forEach((item2,index2) => {
-            if(index !== index2){
-                item2.classList.remove("show-dash-submenu-items");
-            }
-        });
-    });
-});
+// menuItems.forEach((item,index) => {
+//     item.addEventListener("click", () =>{
+//         menu.classList.add("submenu-active");
+//         item.classList.add("show-dash-submenu-items");
+//         menuItems.forEach((item2,index2) => {
+//             if(index !== index2){
+//                 item2.classList.remove("show-dash-submenu-items");
+//             }
+//         });
+//     });
+// });
 
-subMenuTitle.forEach((title) => {
-    title.addEventListener("click", () => {
-        menu.classList.remove("dash-submenu-items");
-    }); 
-});
+// subMenuTitle.forEach((title) => {
+//     title.addEventListener("click", () => {
+//         menu.classList.remove("dash-submenu-items");
+//     }); 
+// });
 
-console.log(menuItems, subMenuTitle);
+// console.log(menuItems, subMenuTitle);
+
+
