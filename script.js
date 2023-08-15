434f5b211d2e82234e1c47623a70c47be1f34a19
Blaben <blabensolutions@gmail.com>
2023-08-15 13:42:14 +0000

    changes

--- script.js
+++ script.js
@@ -23,3 +23,20 @@
 // console.log(menuItems, subMenuTitle);
 
 
+// Function to toggle the visibility of the dropdown menu
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("active"); // Toggle the "active" class
+}
+
+// Attach the toggleDropdown function to the click event of the Department menu item
+var departmentMenuItem = document.querySelector(".dash-menu-item:nth-child(1) a");
+departmentMenuItem.addEventListener("click", toggleDropdown);
+
+// Close the dropdown if the user clicks outside of it
+window.addEventListener("click", function(event) {
+  if (!event.target.matches(".dash-menu-item:nth-child(1) a")) {
+      var dropdown = document.getElementById("myDropdown");
+      dropdown.classList.remove("active"); // Close the dropdown
+  }
+});
